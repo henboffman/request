f0d04a75bac0992b2c6ef08af8622ad706c62210
ben hoffman <benbrent@gmail.com>
2017-04-01 00:28:26 -0400

    added tabs to landing page

--- gameideas/gamedata/App_Start/DurandalBundleConfig.cs
+++ gameideas/gamedata/App_Start/DurandalBundleConfig.cs
@@ -3,9 +3,9 @@ using System.Web.Optimization;
 
 namespace gamedata {
   public class DurandalBundleConfig {
-    public static void RegisterBundles(BundleCollection bundles) {
-      bundles.IgnoreList.Clear();
-      AddDefaultIgnorePatterns(bundles.IgnoreList);
+	public static void RegisterBundles(BundleCollection bundles) {
+	  bundles.IgnoreList.Clear();
+	  AddDefaultIgnorePatterns(bundles.IgnoreList);
 
 	  bundles.Add(
 		new ScriptBundle("~/Scripts/vendor")
@@ -14,27 +14,27 @@ namespace gamedata {
 			.Include("~/Scripts/knockout-{version}.js")
 		);
 
-      bundles.Add(
-        new StyleBundle("~/Content/css")
-          .Include("~/Content/ie10mobile.css")
-          .Include("~/Content/bootstrap.min.css")
-          .Include("~/Content/font-awesome.min.css")
+	  bundles.Add(
+		new StyleBundle("~/Content/css")
+		  .Include("~/Content/ie10mobile.css")
+		  .Include("~/Content/bootstrap.min.css")
+		  .Include("~/Content/font-awesome.min.css")
 		  .Include("~/Content/durandal.css")
-          .Include("~/Content/starterkit.css")
-          .Include("~/Content/app.css")
-        );
-    }
+		  .Include("~/Content/starterkit.css")
+		  .Include("~/Content/app.css")
+		);
+	}
 
-    public static void AddDefaultIgnorePatterns(IgnoreList ignoreList) {
-      if(ignoreList == null) {
-        throw new ArgumentNullException("ignoreList");
-      }
+	public static void AddDefaultIgnorePatterns(IgnoreList ignoreList) {
+	  if(ignoreList == null) {
+		throw new ArgumentNullException("ignoreList");
+	  }
 
-      ignoreList.Ignore("*.intellisense.js");
-      ignoreList.Ignore("*-vsdoc.js");
-      ignoreList.Ignore("*.debug.js", OptimizationMode.WhenEnabled);
-      //ignoreList.Ignore("*.min.js", OptimizationMode.WhenDisabled);
-      //ignoreList.Ignore("*.min.css", OptimizationMode.WhenDisabled);
-    }
+	  ignoreList.Ignore("*.intellisense.js");
+	  ignoreList.Ignore("*-vsdoc.js");
+	  ignoreList.Ignore("*.debug.js", OptimizationMode.WhenEnabled);
+	  //ignoreList.Ignore("*.min.js", OptimizationMode.WhenDisabled);
+	  //ignoreList.Ignore("*.min.css", OptimizationMode.WhenDisabled);
+	}
   }
 }
